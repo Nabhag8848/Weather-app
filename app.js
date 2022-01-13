7b491c5edbc1b75bfa7362e400dc5d0cf8461655
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-01-13 10:59:46 +0530

    Geocoding Service and Error Handling

--- app.js
+++ app.js
@@ -12,13 +12,27 @@
 
 const request = require('request')
 
-const url = 'http://api.weatherapi.com/v1/current.json?key=934151fed33d42c99b485037220201&q=London'
+// const url = 'http://api.weatherapi.com/v1/current.json?key=934151fed33d42c99b485037220201&q=Morbi'
 
-request({url: url,json:true},(error, response)=>{
-    const current = response.body.current
-    console.log('It is Currently',current.temp_c,'degrees out. There is',current.cloud,'% chance of rain')
+// request({url: url,json:true},(error, response)=>{
+//     const current = response.body.current 
+//     console.log('It is Currently',current.temp_c,'degrees out. There is',current.cloud,'% chance of rain')
 
-})
+// })
+
+
+const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?access_token=pk.eyJ1IjoiZGV2ZWxvcGVydHdpdHRlciIsImEiOiJja3k2M3ppcTAwc2FsMnBwcXN0dzA5OHI3In0.KuPw6FbND3nSuG3sOZRBSQ'
+
+request({url: url, json:true},(error, response) =>{
+
+    if(error){
+        console.log('Cannot connect to Weather Service!! Try Again Enabling your Internet!')
+    }else if(response.body.features.length === 0){
+        console.log('Cannot find Location please Search and Try Again!')
+    }else{
+        console.log(response.body.features[0].center[0]); 
+    }
+})  
 
 
 
